fa63a5d2faa90265992655e7dfe8d184fa63ea36
shefket-mustafa <miracle_1@abv.bg>
2025-01-18 18:13:23 +0200

    Add and install Handlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('It works!');
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
+
+app.get('/', (req, res)=>{
+  res.render('home', {layout: false})
+});
+
+app.listen(5001, ()=> console.log('Server is listening on http://localhost:5001...'));
