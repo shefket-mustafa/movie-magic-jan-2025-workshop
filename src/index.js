d9342528011d15345f5fd7f398b6a019f296800d
shefket-mustafa <miracle_1@abv.bg>
2025-01-18 18:46:28 +0200

    Show home page
    modify static asset route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use(express.static('src/public'));
+app.use('/static', express.static('src/public'));
 
 app.get('/', (req, res)=>{
   res.render('home', {layout: false})
