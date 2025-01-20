2e38783b048f62054c4b986f8e277d96b29cdc87
shefket-mustafa <miracle_1@abv.bg>
2025-01-20 17:30:47 +0200

    Show all movies in search page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -4,8 +4,11 @@ import movieService from "../services/movie-service.js";
 const movieController = Router();
 
 movieController.get('/search', (req,res)=>{
-    res.render('search')
-})
+  const movies = movieService.getAll();
+
+  res.render('search', { movies });
+
+});
 
 movieController.get('/create', (req,res)=>{
   res.render('create');
