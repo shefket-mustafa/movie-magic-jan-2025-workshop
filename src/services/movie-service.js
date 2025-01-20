f86378c292c6c7a1e2c482515be84bcf8ffaf362
shefket-mustafa <miracle_1@abv.bg>
2025-01-20 18:06:02 +0200

    Search by genre and year

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -10,6 +10,14 @@ export default {
       if(filter.search){
         result = result.filter(movie => movie.title.toLowerCase().includes(filter.search))
       }
+
+      if(filter.genre){
+        result = result.filter(movie => movie.genre.toLowerCase() === filter.genre)
+      }
+
+      if(filter.year){
+        result = result.filter(movie => movie.year === filter.year)
+      }
       return result;
     },
 
