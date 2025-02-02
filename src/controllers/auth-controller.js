2e260f19b40dce257c973b9d01209256ff47e7dc
shefket-mustafa <miracle_1@abv.bg>
2025-02-02 16:35:39 +0200

    Successful registration data sent to register endpoint

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -4,6 +4,13 @@ const authController = Router();
 
 authController.get('/register', (req,res) => {
     res.render('auth/register');
-})
+});
+
+authController.post('/register', async (req,res) => {
+    const userData = req.body;
+    console.log(userData);
+
+    res.end();
+});
 
 export default authController;
