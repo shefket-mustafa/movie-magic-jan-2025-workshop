99dbaa7b22ee9958d5781741297279b83ca3f470
shefket-mustafa <miracle_1@abv.bg>
2025-02-02 18:41:12 +0200

    Add user model

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authService from '../services/auth-service.js';
 
 const authController = Router();
 
@@ -8,7 +9,8 @@ authController.get('/register', (req,res) => {
 
 authController.post('/register', async (req,res) => {
     const userData = req.body;
-    console.log(userData);
+    
+    await authService.register(userData);
 
     res.end();
 });
