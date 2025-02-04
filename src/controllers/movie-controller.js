bdc9ce7e56850fe3a52b6473043f636c48dbc5fa
shefket-mustafa <miracle_1@abv.bg>
2025-02-04 21:43:58 +0200

    Show dynamic select values

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -4,72 +4,89 @@ import castService from "../services/cast-service.js";
 
 const movieController = Router();
 
-movieController.get('/search', async (req, res) => {
-    const filter = req.query;
-    const movies = await movieService.getAll(filter);
+movieController.get("/search", async (req, res) => {
+  const filter = req.query;
+  const movies = await movieService.getAll(filter);
 
-    res.render('search', { movies, filter });
+  res.render("search", { movies, filter });
 });
 
-movieController.get('/create', (req, res) => {
-    res.render('create');
+movieController.get("/create", (req, res) => {
+  res.render("create");
 });
 
-movieController.post('/create', async (req, res) => {
-    const newMovie = req.body;
-    const userId = req.user?.id;
+movieController.post("/create", async (req, res) => {
+  const newMovie = req.body;
+  const userId = req.user?.id;
 
-    await movieService.create(newMovie, userId); 
+  await movieService.create(newMovie, userId);
 
-    res.redirect('/');
+  res.redirect("/");
 });
 
-movieController.get('/:movieId/details', async (req, res) => {
-    
+movieController.get("/:movieId/details", async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOneWithCasts(movieId);
+  // const casts = await castService.getAll(movie.casts);
 
-    const movieId = req.params.movieId;
-    const movie = await movieService.getOneWithCasts(movieId);
-    // const casts = await castService.getAll(movie.casts);
-    
-    
-    const isCreator = movie.creator && movie.creator.toString() === req.user.id;
+  const isCreator = movie.creator && movie.creator.toString() === req.user.id;
 
-    res.render('movie/details', { movie, isCreator });
+  res.render("movie/details", { movie, isCreator });
 });
 
-movieController.get('/:movieId/attach-cast', async (req, res) => {
-    const movieId = req.params.movieId;
-    const movie = await movieService.getOne(movieId);
-    const casts = await castService.getAll({exclude: movie.casts});
+movieController.get("/:movieId/attach-cast", async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOne(movieId);
+  const casts = await castService.getAll({ exclude: movie.casts });
 
-    res.render('movie/attach-cast', { movie, casts });
+  res.render("movie/attach-cast", { movie, casts });
 });
 
-movieController.post('/:movieId/attach-cast', async (req, res) => {
-    const castId = req.body.cast;
-    const movieId = req.params.movieId;
-    await movieService.attachCast(movieId, castId);
+movieController.post("/:movieId/attach-cast", async (req, res) => {
+  const castId = req.body.cast;
+  const movieId = req.params.movieId;
+  await movieService.attachCast(movieId, castId);
 
-    res.redirect(`/movies/${movieId}/details`);
+  res.redirect(`/movies/${movieId}/details`);
 });
 
-movieController.get('/:movieId/delete', async (req,res) => {
-    const movieId = req.params.movieId;
+movieController.get("/:movieId/delete", async (req, res) => {
+  const movieId = req.params.movieId;
 
-    const movie = await movieService.getOne(movieId);
-    if(!movie.creator?.equals(req.user?.id)){
-        return res.redirect('/404');
-    }
- 
+  const movie = await movieService.getOne(movieId);
+  if (!movie.creator?.equals(req.user?.id)) {
+    return res.redirect("/404");
+  }
 
-    await movieService.delete(movieId);
-    res.redirect('/');
+  await movieService.delete(movieId);
+  res.redirect("/");
 });
 
-movieController.get('/:movieId/edit', async (req,res) => {
-    const movieId = req.params.movieId;
-    const movie = await movieService.getOne(movieId);
-    res.render('movie/edit', { movie });
+function getCategoriesViewData(category){
+    const categoriesMap = {
+        "tv-show": 'TV SHOW' ,
+       "animation": 'Animation',
+       "movie": 'Movie',
+        "documentary": 'Documentary',
+        "short-film": 'Short Film'}
+
+
+const categories = Object.keys(categoriesMap).map(value => ({
+    value,
+     label: categoriesMap[value],
+     selected: value === category ? 'selected': '',
+    }));
+    return categories;
+};
+
+
+movieController.get("/:movieId/edit", async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOne(movieId);
+
+const categories = getCategoriesViewData(movie.category);
+
+  res.render("movie/edit", { movie, categories });
 });
 
 export default movieController;
