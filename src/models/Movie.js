e6237db51eab4615abcc935065f2c85829b09a7d
shefket-mustafa <miracle_1@abv.bg>
2025-02-05 19:04:42 +0200

    Movie model validation added

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,27 +1,72 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, model, Types } from "mongoose";
 
 // Create schema
 const movieSchema = new Schema({
-    title: String,
-    category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
-    casts: [{
-        type: Types.ObjectId,
-        ref: 'Cast'
-    }],
-    creator: {
-        type: Types.ObjectId,
-        ref: 'User',
-    }
+  title: {
+    type: String,
+    required: [true, "Title is required!"],
+    minLength: [5, "Title should be at least 5 characters long!"],
+    maxLength: 250,
+    match: [
+      /^[a-zA-Z 0-9]+$/,
+      "Title should be alphanumeric, diigits and whitespaces only!",
+    ],
+  },
+  category: String,
+  genre: {
+    type: String,
+    required: [true, "Genre is required!"],
+    minLength: [5, "Genre should be at least 5 characters long!"],
+    maxLength: 250,
+    match: [
+      /^[a-zA-Z 0-9]+$/,
+      "Genre should be alphanumeric, diigits and whitespaces only!",
+    ],
+  },
+
+  director: {
+    type: String,
+    required: [true, "Director is required!"],
+    minLength: [5, "Director should be at least 5 characters long!"],
+    maxLength: 250,
+    match: [
+      /^[a-zA-Z 0-9]+$/,
+      "Director should be alphanumeric, diigits and whitespaces only!",
+    ],
+  },
+  year: {
+    type: Number,
+    min: 1900,
+    max: 2025,
+  },
+  imageUrl: {
+    type: String,
+    match: /^https?:\/\//,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  description: {
+    type: String,
+    minLength: 20,
+    match: /^[a-zA-Z 0-9]+$/ 
+  },
+  casts: [
+    {
+      type: Types.ObjectId,
+      ref: "Cast",
+    },
+  ],
+  creator: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
 });
 
 // Create model
-const Movie = model('Movie', movieSchema);
+const Movie = model("Movie", movieSchema);
 
 // Export model
 export default Movie;
